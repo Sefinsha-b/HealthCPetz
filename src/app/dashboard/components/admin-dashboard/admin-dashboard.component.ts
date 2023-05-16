4d3d0a3534fd66f5748ef14c408c3d124fd6bdab
Sefinsha-b <Sefinsha4@gmail.com>
2023-05-16 21:55:41 +0530

    Push Notification

--- src/app/dashboard/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/dashboard/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
-
-  constructor() { }
+  totalVets: any;
+  totalPets: any;
+  totalAppointment: any
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getTotalAppointment();
+    this.getVets();
+    this.getPets();
   }
 
+  getTotalAppointment() {
+    this.http.get('http://localhost:3000/Appointments').subscribe((resp) => {
+      this.totalAppointment = resp
+      console.log(resp)
+    })
+
+  }
+  getVets() {
+    this.http.get('http://localhost:3000/Vets').subscribe((resp) => {
+      this.totalVets = resp
+      console.log(resp)
+    })
+
+  }
+  getPets() {
+    this.http.get('http://localhost:3000/Pets').subscribe((resp) => {
+      this.totalPets = resp
+      console.log(resp)
+    })
+
+  }
 }
